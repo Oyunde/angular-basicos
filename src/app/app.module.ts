daa86bf6c1e7ea2b98b3eefe541d57c4f6d0b4da
Oyunde <v.rojasy@alumnos.duoc.cl>
2021-10-06 22:14:45 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// tarea
+// Crear un modulo llamado ContadorModule.
+//hacer declaraciones y exportaciones
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
